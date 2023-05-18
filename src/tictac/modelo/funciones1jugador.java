59d5a1672f3863d041735983e3afa7da34897e5b
rodrigopalomino <117995998+rodrigopalomino@users.noreply.github.com>
2023-05-18 10:27:43 -0500

    Update funciones1jugador.java
    actualizando desde git

--- src/tictac/modelo/funciones1jugador.java
+++ src/tictac/modelo/funciones1jugador.java
@@ -19,6 +19,7 @@ public class funciones1jugador {
 	//iniciando variables
 	private int cont;
 	private boolean player1 = true;
+	boolean game_over=false;
 	JButton[][] botones;
 	JButton btnReset;
 
@@ -62,108 +63,104 @@ public class funciones1jugador {
 		
 		btnReset = new JButton("reset");
 		btnReset.setBounds(303, 250, 89, 23);
-		//btnReset.setVisible(false);
 		getJugadores1().getContentPane().add(btnReset);
 		btnReset.addMouseListener(new MouseAdapter() {
 			@Override public void mouseClicked(MouseEvent e) { reiniciar(); }});
 
-		
 	}
 		
-
-	protected void btn_mouseClicked(JButton btn, int i, int y) {
+	protected void btn_mouseClicked(JButton btn, int finalI, int finalY) {
+		Random rm = new Random(3);
 		
-		if(verificarGanadorPlayer(i, y)) {
-	    	getJugadores1().lblPlayer.setText("ganador");
-	    }
 		
-		btn.setText("x");
-	    cont++;
-	    player1 = false;
-	    getJugadores1().lblPlayer.setText("Player 2");
-	    
-	    if(verificarGanadorPlayer(i, y)) {
-	    	getJugadores1().lblPlayer.setText("ganador player");
-	    }
-	    
-	    
-	    /*
-	    if( verificarGanador(i,y) ) {
+		if( !game_over ) {
+			//turno jugador 1
+			btn.setText("x");
 			
-			if( getJugadores1().lblPlayer.getText().equals("Player 1") ) {
-				getJugadores1().lblPlayer.setText("Player 2");
-			}  
-			if( getJugadores1().lblPlayer.getText().equals("Player 2") ) {
-				getJugadores1().lblPlayer.setText("Player 1");
-			} 
+			if(verificarGanador(finalI, finalY, "x")){
+				getJugadores1().lblTurno.setText("ganador 1");
+			}
 			
-			getJugadores1().lblTurno.setText("Ganador");
-			getJugadores1().btnCerrar.setVisible(true);
-			btnReset.setVisible(true);
-		} else if (cont==9) {
-			getJugadores1().lblTurno.setText("empate");
+			boolean bot=true;
+		
+			while( bot ) {
+				int n1 = rm.nextInt(3);
+				int n2 = rm.nextInt(3);
+				if(botones[n1][n2].getText().isEmpty()) {
+					botones[n1][n2].setText("o");
+					bot=false;
+					if( ganadorBot("o") ) {
+						getJugadores1().lblTurno.setText("ganador bot");
+						game_over=true;
+					}
+				}
+				
+			}
+			
+			
+			
+		}else {
+			System.out.print("perdiste");
 			
 		}
-	    */
-	    Random random = new Random();
-	    while (true) {
-	        int n1 = random.nextInt(3);
-	        int n2 = random.nextInt(3);
-
-	        if (botones[n1][n2].getText().isEmpty()) {
-	            botones[n1][n2].setText("o");
-	            cont++;
-	            getJugadores1().lblPlayer.setText("Player 1");
-	            player1 = true;
-	            break;
-	        }
-	    
-
-	    }
-		if(verificarGanadorBot(i, y)) {
-			getJugadores1().lblPlayer.setText("ganador bot");
-		}
-
 		
 	}
-	
-	
-	private boolean verificarGanadorPlayer(int i, int y) {
-		String simbolo = "x";
+
+	private boolean ganadorBot(String simbolo) {
+		//{0,0}{0,1}{0,2}
+		//{1,0}{1,1}{1,2}
+		//{2,0}{2,1}{2,2}
 		
-
-        if (botones[i][0].getText().equals(simbolo) && 
-       		botones[i][1].getText().equals(simbolo) &&
-       		botones[i][2].getText().equals(simbolo)) {
-            return true;
-        }
-
-
-        if (botones[0][y].getText().equals(simbolo) &&
-        	botones[1][y].getText().equals(simbolo) &&
-        	botones[2][y].getText().equals(simbolo)) {
-            return true;
-        }
-
-
-        if (i == y && 
-       		botones[0][0].getText().equals(simbolo) &&
-       		botones[1][1].getText().equals(simbolo) &&
-       		botones[2][2].getText().equals(simbolo)) {
-            return true;
-        }
-        
-        if (botones[0][2].getText().equals(simbolo) &&
-    		botones[1][1].getText().equals(simbolo) &&
-       		botones[2][0].getText().equals(simbolo)) {
-                return true;
-            }
-        
+		
+		if (botones[0][0].getText().equals(simbolo) && 
+	       	botones[0][1].getText().equals(simbolo) &&
+	       	botones[0][2].getText().equals(simbolo)) {
+	        	return true;
+	    }
+		if (botones[1][0].getText().equals(simbolo) && 
+		    botones[1][1].getText().equals(simbolo) &&
+		    botones[1][2].getText().equals(simbolo)) {
+		        	return true;
+		}
+		if (botones[2][0].getText().equals(simbolo) && 
+		    botones[2][1].getText().equals(simbolo) &&
+		    botones[2][2].getText().equals(simbolo)) {
+		        	return true;
+		}
+		
+		if (botones[0][0].getText().equals(simbolo) && 
+		    botones[1][0].getText().equals(simbolo) &&
+		    botones[2][0].getText().equals(simbolo)) {
+		        	return true;
+		}
+		if (botones[0][1].getText().equals(simbolo) && 
+			botones[1][1].getText().equals(simbolo) &&
+			botones[2][1].getText().equals(simbolo)) {
+			        	return true;
+		}
+		if (botones[0][2].getText().equals(simbolo) && 
+			botones[1][2].getText().equals(simbolo) &&
+			botones[2][2].getText().equals(simbolo)) {
+			        	return true;
+		}
+		if (botones[0][0].getText().equals(simbolo) && 
+				botones[1][1].getText().equals(simbolo) &&
+				botones[2][2].getText().equals(simbolo)) {
+				        	return true;
+		}
+		if (botones[2][0].getText().equals(simbolo) && 
+				botones[1][1].getText().equals(simbolo) &&
+				botones[0][2].getText().equals(simbolo)) {
+				        	return true;
+		}
+		
+	
+		
 		return false;
 	}
-	
-	private boolean verificarGanadorBot(int i, int y) {
-		String simbolo = "o";
+
+	private boolean verificarGanador(int i, int y, String simbolo) {
+		
 		
 
         if (botones[i][0].getText().equals(simbolo) && 
